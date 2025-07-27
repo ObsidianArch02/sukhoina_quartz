27b4e8172bd74109ae48d0c2472177e60fb38510
ObsidianArch02 <72397119+ObsidianArch02@users.noreply.github.com>
2025-07-27 13:35:05 +0800

    Quartz sync: Jul 27, 2025, 1:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,16 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "sukhoina_quartz",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    // locale: "en-US",
+    locale: "zh-CN",
+    baseUrl: "obsidianarch02.github.io/sukhoina_quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
